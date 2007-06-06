7185dd3549b17ac21f0fc80e9fee12a2864a4180
rcassolato <rcassolato@2507c435-082f-0410-bf6c-41b6020ff1ed>
2007-06-06 02:45:59 +0000

    Version By MyEclipse

---
+++ trunk/sctmf/src/cassolato/rafael/sctmf/model/services/ValidaSequencia.java
@@ -0,0 +1,157 @@
+/*
+ * ValidaSequencia.java
+ *
+ * Created on 10 de Maio de 2007, 09:50
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package cassolato.rafael.sctmf.model.services;
+
+import cassolato.rafael.sctmf.model.pojo.AFD;
+import cassolato.rafael.sctmf.model.pojo.AFND;
+import cassolato.rafael.sctmf.model.pojo.Estado;
+import cassolato.rafael.sctmf.model.pojo.ModeloFormal;
+import cassolato.rafael.sctmf.model.pojo.Simbolo;
+import cassolato.rafael.sctmf.model.pojo.Transicao;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author rafael2009_00
+ */
+public class ValidaSequencia implements Validacao {
+    
+    private static ValidaSequencia singleton;
+    
+    /** Creates a new instance of ValidaSequencia */
+    private ValidaSequencia() {
+    }
+    
+    public void valida(ModeloFormal fm, String sequencia) {
+        boolean status = false;
+        
+        if(fm instanceof AFD )
+            status = this.valida((AFD)fm, sequencia);
+        else if(fm instanceof AFND )
+            status = this.valida((AFND)fm, sequencia);
+        
+        this.sendMessage(status);
+    }
+    
+    /**
+     * Metodo que faz a validação de uma sequencia.
+     *
+     * @param afd - Automato finito deterministico
+     * @param sequencia - Sequencia digitada pelo usuário
+     *
+     * @return boolean;
+     */
+    private boolean valida(AFD afd, String sequencia) {
+        // Estado Inicial
+        Estado estadoAtual = afd.getEstadoInicial();
+        
+        for(char c : sequencia.toCharArray())  {
+            label : {
+                for(Transicao t : afd.getTransicoes()) {
+                    Estado e = t.getEstOri();
+                    Simbolo s = t.getSimbolo();
+                    if(estadoAtual.getNome().equals(e.getNome())
+                       &&c==(s.getNome())) {
+                        
+                        estadoAtual = t.getEstDest();
+                        break label;
+                    }
+                }
+                
+                // caso nao exista nenhuma transicao que o leve ao proximo estado
+                return false;
+            }
+            
+        }
+        
+        // Verifica se o Estado atual Ã© algum
+        // Dos estados finais
+        for(Estado x : afd.getEstadosFinais())
+            if(estadoAtual.getNome().equals(x.getNome()))
+                return true;
+        
+        return false;
+        
+    }
+    
+    private boolean valida(AFND afnd, String sequencia) {
+        // Estados que podem estar ativo no AFND
+        Map<String, Estado> estadosAtivos = new HashMap<String, Estado>();
+        // Estados ativos - Var Auxiliar
+        Map<String, Estado> auxEA = new HashMap<String, Estado>();
+        
+        // Preeche os estados ativos com os estados iniciais.
+        for(Estado e : afnd.getEstadosIniciais())
+            estadosAtivos.put(e.getNome(), e);
+        
+        // Percorre a sequencia
+        for(char c : sequencia.toCharArray()) {            
+            // Percorre as transicoes
+            for(Transicao t : afnd.getTransicoes()) {
+                Estado e = t.getEstOri();
+                Simbolo s = t.getSimbolo();
+                
+                Estado aux = estadosAtivos.get(e.getNome());
+                // Verifica se existe uma transicao cadastrada
+                // que ativa um estado novo
+                if(aux!=null&&c==s.getNome().charValue()) {
+                    Estado ed = t.getEstDest();
+                    auxEA.put(ed.getNome(), ed);
+                }
+                
+            }
+            
+            if(auxEA.size()==0)  // retorna falso caso nao existe uma transicao
+                return false;    // que leve um determinado estado a outro
+            
+            // Atribui o clone do objeto auxEA para os estados ativos
+            // e limpa o auxEA
+            try {
+                estadosAtivos = (Map) auxEA.getClass().
+                        getMethod("clone").invoke(auxEA);
+                auxEA.clear();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            
+        }
+        
+        // Vefirica se existe algum estado ativo
+        // contem dentro dos estados finais.
+        for(Estado ef : afnd.getEstadosFinais())
+            if(estadosAtivos.containsKey(ef.getNome()))
+                return true;
+        
+        return false;
+    }
+    
+    private void sendMessage(boolean isOK) {
+        if(isOK)
+            javax.swing.JOptionPane.showMessageDialog(null,
+                    "Seqüência ACEITA", "!! OK !!",
+                    javax.swing.JOptionPane.INFORMATION_MESSAGE);
+        else
+            javax.swing.JOptionPane.showMessageDialog(null,
+                    "Seqüência REJEITADA", "!! Atenção !!",
+                    javax.swing.JOptionPane.WARNING_MESSAGE);
+        
+    }
+    
+    public static ValidaSequencia getInstance() {
+        return singleton;
+    }
+    
+    static {
+        singleton =
+                new ValidaSequencia();
+    }
+    
+}
